a0ba66e2f9e65bf72049fe84c76301b3c66c67ad
Aaron Hampshire <aaron.hampshire@gmail.com>
2019-04-09 22:09:08 -0700

    Add sass and compile with gulp

--- LoginPage/gulpfile.js
+++ LoginPage/gulpfile.js
@@ -1,17 +1,26 @@
 // Gulp.js configuration
 var gulp = require('gulp');
 
-gulp.task('css', function () {
-    var postcss = require('gulp-postcss');
-    var tailwindcss = require('tailwindcss');
-    var rename = require('gulp-rename');
-    return gulp.src('style/tailwind.css')
+// compile sass and tailwind
+gulp.task('sass', function () {
+  var sass = require('gulp-sass');
+  sass.compiler = require('node-sass');
+  var postcss = require('gulp-postcss');
+  let cleanCSS = require('gulp-clean-css');
+  var tailwindcss = require('tailwindcss');
+  var stripCssComments = require('gulp-strip-css-comments');
+  var rename = require('gulp-rename');
+  return gulp.src('style/tailwind.scss')
+      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
       .pipe(postcss([
         tailwindcss('./style/tailwind.js'),
         require('autoprefixer')
-      ]))
+        ]))
+      .pipe(cleanCSS())
+      .pipe(stripCssComments({preserve: false}))
       .pipe(rename("style.css"))
-      .pipe(gulp.dest('src/'));
-  });
+      .pipe(gulp.dest('./src/'));
+});
 
-gulp.task('default', gulp.series('css'));
+gulp.task('default', gulp.series('sass'));
+
